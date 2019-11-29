676f47d5f1bcff98f72b5c0aeac8c8ab72a524d4
Paul-MEDARD-SN2 <57522211+Paul-MEDARD-SN2@users.noreply.github.com>
2019-11-29 15:54:10 +0100

    BDD export (2.0)

---
+++ Tache Personnel/Paul/bbd_blindtest.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 29 Novembre 2019 à 14:50
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bbd_blindtest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `artiste`
+--
+
+CREATE TABLE `artiste` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `artiste`
+--
+
+INSERT INTO `artiste` (`id`, `nom`) VALUES
+(1, 'Nekfeu'),
+(2, 'Ninho');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE `joueur` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `musique`
+--
+
+CREATE TABLE `musique` (
+  `id_musique` int(10) UNSIGNED NOT NULL COMMENT '\n',
+  `nom` varchar(30) NOT NULL,
+  `artiste_id` int(10) UNSIGNED NOT NULL,
+  `theme_idtheme` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `musique`
+--
+
+INSERT INTO `musique` (`id_musique`, `nom`, `artiste_id`, `theme_idtheme`) VALUES
+(1, 'Ma dope', 1, 1),
+(2, 'Maman ne le sait pas', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `musique_nonjoue`
+--
+
+CREATE TABLE `musique_nonjoue` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `artiste_id` int(10) UNSIGNED NOT NULL,
+  `theme_idtheme` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `theme`
+--
+
+CREATE TABLE `theme` (
+  `idtheme` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(30) DEFAULT NULL,
+  `miniature` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `theme`
+--
+
+INSERT INTO `theme` (`idtheme`, `nom`, `miniature`) VALUES
+(1, 'RAP', NULL),
+(2, 'Année 80', NULL),
+(3, 'Année 2000', NULL),
+(4, 'R&B', NULL);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `artiste`
+--
+ALTER TABLE `artiste`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `musique`
+--
+ALTER TABLE `musique`
+  ADD PRIMARY KEY (`id_musique`),
+  ADD KEY `fk_musique_artiste1_idx` (`artiste_id`),
+  ADD KEY `fk_musique_theme1_idx` (`theme_idtheme`);
+
+--
+-- Index pour la table `musique_nonjoue`
+--
+ALTER TABLE `musique_nonjoue`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_musique_nonjoue_artiste1_idx` (`artiste_id`),
+  ADD KEY `fk_musique_nonjoue_theme1_idx` (`theme_idtheme`);
+
+--
+-- Index pour la table `theme`
+--
+ALTER TABLE `theme`
+  ADD PRIMARY KEY (`idtheme`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `artiste`
+--
+ALTER TABLE `artiste`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `musique`
+--
+ALTER TABLE `musique`
+  MODIFY `id_musique` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '\n', AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `musique_nonjoue`
+--
+ALTER TABLE `musique_nonjoue`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `theme`
+--
+ALTER TABLE `theme`
+  MODIFY `idtheme` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `musique`
+--
+ALTER TABLE `musique`
+  ADD CONSTRAINT `fk_musique_artiste1` FOREIGN KEY (`artiste_id`) REFERENCES `artiste` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_musique_theme1` FOREIGN KEY (`theme_idtheme`) REFERENCES `theme` (`idtheme`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `musique_nonjoue`
+--
+ALTER TABLE `musique_nonjoue`
+  ADD CONSTRAINT `fk_musique_nonjoue_artiste1` FOREIGN KEY (`artiste_id`) REFERENCES `artiste` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_musique_nonjoue_theme1` FOREIGN KEY (`theme_idtheme`) REFERENCES `theme` (`idtheme`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
